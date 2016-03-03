75e6d18757e9d5a64482bb344b9c4d4efbdb05a6
Jeffrey Jen <yihungjen@gmail.com>
2016-03-03 02:01:24 +0000

    NIT: show skipping cert install when not a Docker Engine

--- lib/machine/host.go
+++ lib/machine/host.go
@@ -18,6 +18,7 @@ type Host struct {
 
 func (h *Host) InstallDockerEngineCertificate(host string, altname ...string) error {
 	if !h.IsDocker { // Not processing because not a Docker Engine
+		fmt.Println(host, "- skipping Docker Certificate Install")
 		return nil
 	}
 
@@ -27,14 +28,12 @@ func (h *Host) InstallDockerEngineCertificate(host string, altname ...string) er
 		"127.0.0.1",
 	}
 	subAltNames = append(subAltNames, altname...)
-
 	fmt.Println(host, "- generate cert for subjects -", subAltNames)
 
 	CA, Cert, Key, err := cert.GenerateServerCertificate(h.CertPath, h.Organization, subAltNames)
 	if err != nil {
 		return err
 	}
-
 	ssh_config := ssh.Config{
 		User:   h.User,
 		Server: host,
