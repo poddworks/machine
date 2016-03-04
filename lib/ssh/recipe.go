b200e3193e6ab6465f86cd884d82192e088a2e82
Jeffrey Jen <yihungjen@gmail.com>
2016-03-04 18:21:43 +0000

    NEW: introduce recipe for playbook

---
+++ lib/ssh/recipe.go
@@ -0,0 +1,57 @@
+package ssh
+
+import (
+	"fmt"
+	path "path/filepath"
+)
+
+type Recipe struct {
+	Archive   `yaml:"archive"`
+	Provision []Provision `yaml:"provision"`
+}
+
+type Archive struct {
+	RemoteDir string   `yaml:"dir"`
+	Local     []string `yaml:"local"`
+}
+
+func (a Archive) Send(cmdr Commander) error {
+	for _, local := range a.Local {
+		dst := path.Join(a.RemoteDir, path.Base(local))
+		if err := cmdr.CopyFile(local, dst, 0644); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+type Provision struct {
+	Name    string   `yaml:"name"`
+	Ok2fail bool     `yaml:"ok2fail"`
+	Action  []Action `yaml:"action"`
+}
+
+type Action struct {
+	Cmd    string `yaml:"cmd"`
+	Script string `yaml:"script"`
+	Sudo   bool   `yaml:"sudo"`
+}
+
+func (a Action) Act(cmdr Commander) (output <-chan Response, err error) {
+	if a.Sudo {
+		defer cmdr.Sudo().StepDown()
+	}
+	switch {
+	case a.Cmd != "":
+		output, err = cmdr.Stream(a.Cmd)
+		break
+	case a.Script != "":
+		dst := path.Join("/tmp", path.Base(a.Script))
+		err = cmdr.CopyFile(a.Script, dst, 0644)
+		if err == nil {
+			output, err = cmdr.Stream(fmt.Sprintf("bash %s", dst))
+		}
+		break
+	}
+	return
+}
