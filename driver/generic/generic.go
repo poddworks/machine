83de1a0ea245044fc9ddd6ce85fdbbea96cdc076
Jeffrey Jen <yihungjen@gmail.com>
2016-10-17 18:30:10 +0000

    Allow user assign generic target instance driver

--- driver/generic/generic.go
+++ driver/generic/generic.go
@@ -13,6 +13,8 @@ func NewCreateCommand() cli.Command {
 		Name:  "generic",
 		Usage: "Setup Machine to use Docker Engine",
 		Flags: []cli.Flag{
+			cli.BoolFlag{Name: "no-install", Usage: "Skip Docker Engine Installation"},
+			cli.StringFlag{Name: "driver", Value: "generic", Usage: "Assign driver for Docker Engine"},
 			cli.StringFlag{Name: "host", Usage: "Host to install Docker Engine"},
 			cli.StringSliceFlag{Name: "altname", Usage: "Alternative name for Host"},
 		},
@@ -20,18 +22,22 @@ func NewCreateCommand() cli.Command {
 			defer mach.InstList.Dump()
 
 			var (
-				org, certpath, _ = mach.ParseCertArgs(c)
-
 				user     = c.GlobalString("user")
 				cert     = c.GlobalString("cert")
+				driver   = c.String("driver")
 				hostname = c.String("host")
 				altnames = c.StringSlice("altname")
 
 				name    = c.Args().First()
 				addr, _ = net.ResolveTCPAddr("tcp", hostname+":2376")
 
-				inst = mach.NewDockerHost(org, certpath, user, cert)
+				noInstall = c.Bool("no-install")
 			)
+
+			org, certpath, err := mach.ParseCertArgs(c)
+			if err != nil {
+				return cli.NewExitError(err.Error(), 1)
+			}
 
 			if name == "" {
 				return cli.NewExitError("Required argument `name` missing", 1)
@@ -43,15 +49,19 @@ func NewCreateCommand() cli.Command {
 				return cli.NewExitError("Missing required remote auth info", 1)
 			}
 
-			if err := inst.InstallDockerEngine(hostname); err != nil {
-				return cli.NewExitError(err.Error(), 1)
+			inst := mach.NewDockerHost(org, certpath, user, cert)
+
+			if !noInstall {
+				if err := inst.InstallDockerEngine(hostname); err != nil {
+					return cli.NewExitError(err.Error(), 1)
+				}
 			}
 			if err := inst.InstallDockerEngineCertificate(hostname, altnames...); err != nil {
 				return cli.NewExitError(err.Error(), 1)
 			}
 			mach.InstList[name] = &mach.Instance{
 				Id:         name,
-				Driver:     "generic",
+				Driver:     driver,
 				DockerHost: addr,
 				Host:       hostname,
 				AltHost:    altnames,
